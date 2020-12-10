b9f14cec85beaf1bf2a3010786b116d3f822c8c5
jmlegend78 <73984272+jmlegend78@users.noreply.github.com>
2020-12-10 12:38:26 +0800

    fb initial commit

---
+++ conn.php
@@ -0,0 +1,10 @@
+<?php
+	//initialization
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$databasename  = "dbFacebook";
+
+	//connection php to mysql
+	$connect = mysqli_connect($servername,$username,$password,$databasename);
+?>
